168207ed60f857e8461ea69b65a94ff08f8d6963
WorkaHolic <baileypark@kakao.com>
2024-04-20 02:26:07 +0900

    update: Autowired

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -2,6 +2,7 @@ package io.hhplus.tdd.point;
 
 import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,8 +10,10 @@ import java.util.List;
 @Service
 public class PointService {
 
-    UserPointTable userPointTable = new UserPointTable();
-    PointHistoryTable pointHistoryTable = new PointHistoryTable();
+    @Autowired
+    UserPointTable userPointTable;
+    @Autowired
+    PointHistoryTable pointHistoryTable;
 
     public UserPoint pointById(long id) {
         return userPointTable.selectById(id);
@@ -21,22 +24,25 @@ public class PointService {
     }
 
     public UserPoint chargeById(long id, long amount) {
-        pointHistoryTable.insert(id, amount, TransactionType.CHARGE, System.currentTimeMillis());
         UserPoint userPoint = userPointTable.selectById(id);
         long point = userPoint.point();
         point += amount;
+        pointHistoryTable.insert(id, amount, TransactionType.CHARGE, System.currentTimeMillis());
         return userPointTable.insertOrUpdate(id, point);
     }
 
 
     public UserPoint useById(long id, long amount) {
-        pointHistoryTable.insert(id, amount, TransactionType.USE, System.currentTimeMillis());
         UserPoint userPoint = userPointTable.selectById(id);
         long point = userPoint.point();
         if (point < amount) {
+            /*
+            * 예외처리 진행
+            * */
             System.out.println("lack of points");
             return userPoint;
         }
+        pointHistoryTable.insert(id, amount, TransactionType.USE, System.currentTimeMillis());
         point -= amount;
         return userPointTable.insertOrUpdate(id, point);
     }
